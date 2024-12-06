f2637204e941f65ad94d8fdd60f025440ea3b017
testeSen132 <cahosped@gmail.com>
2024-12-06 06:16:02 -0300

    testes

--- pages/acessorios/bones/paginas/script.js
+++ pages/acessorios/bones/paginas/script.js
@@ -1,6 +1,6 @@
 function trocarImagem() {
     var imagem = document.getElementById("imagem");
-    
+
     // Verificar qual imagem está sendo exibida e trocar
     if (imagem.src.match("imagem1")) {
         imagem.src = "imagens/"; // Substitua pelo caminho da sua segunda imagem
@@ -16,26 +16,26 @@ const count = document.getElementById("counter")
 
 let myCount = 0
 
-add.addEventListener("click", ()=>{
+add.addEventListener("click", () => {
     myCount += 1
     count.innerText = myCount
 })
 
-sub.addEventListener("click", ()=>{
+sub.addEventListener("click", () => {
 
-    if(myCount >= 1){
+    if (myCount >= 1) {
         myCount -= 1
-        count.innerText = myCount    
+        count.innerText = myCount
     }
 
 })
 
 const swatches = document.querySelectorAll('.swatch-item');
-        const selectedSize = document.getElementById('selected-size');
+const selectedSize = document.getElementById('selected-size');
 
-        swatches.forEach(swatch => {
-            swatch.addEventListener('click', () => {
-                const size = swatch.getAttribute('data-size');
-                selectedSize.textContent = ` ${size}`;
-            });
-        });
+swatches.forEach(swatch => {
+    swatch.addEventListener('click', () => {
+        const size = swatch.getAttribute('data-size');
+        selectedSize.textContent = ` ${size}`;
+    });
+});

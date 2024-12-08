0193010610d3331253a26b006a95e89cf88d0f4b
testeSen132 <cahosped@gmail.com>
2024-12-08 09:18:19 -0300

    o uso de estilo nas tags impossibilita a responsividade nas demais paginas sem que seja alterada toda a estrutura, da categoria de produtos e dos produtos individuais

--- pages/feminino/camisa/script.js
+++ pages/feminino/camisa/script.js
@@ -29,3 +29,35 @@ sub.addEventListener("click", ()=>{
     }
 
 })
+
+// responsividade header
+
+document.addEventListener("DOMContentLoaded", () => {
+    // Menu hamburguer
+    const menuToggle = document.getElementById("menu-toggle");
+    const menu = document.getElementById("menu");
+  
+    menuToggle.addEventListener("click", () => {
+      menu.classList.toggle("active");
+    });
+  
+    // Submenu toggle
+    const submenuToggles = document.querySelectorAll(".submenu-toggle");
+  
+    submenuToggles.forEach((toggle) => {
+      toggle.addEventListener("click", () => {
+        const currentSubmenu = toggle.nextElementSibling;
+  
+      document.querySelectorAll(".submenu").forEach((submenu) =>{
+        if (submenu !== currentSubmenu){
+          submenu.classList.remove("expanded")
+        }
+      })
+  
+        // fechar as outras categorias
+  
+        currentSubmenu.classList.toggle("expanded");
+      });
+    });
+  });
+  
